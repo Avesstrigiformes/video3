e751d442704d335ba5b2bb3aadd90a6cba60e6cc
nameless <112693247+Avesstrigiformes@users.noreply.github.com>
2025-04-03 15:08:51 +0300

    Small Update

--- thumbnail.py
+++ thumbnail.py
@@ -17,7 +17,7 @@ class Thumbnail(Scene):
             return glow_group
         a = MathTex(r"\int dt").scale(2)
         b = brachistochrone().set_color_by_gradient([PURE_RED, PURE_BLUE]).set_sheen_direction(UR)
-        c = Tex(r"Maximize").scale(2).set_color(PURE_RED).shift(DL*2.7)
+        c = Tex(r"Minimize").scale(2).set_color(PURE_RED).shift(DL*2.7)
         d = Tex(r"Constant").scale(2).set_color(PURE_BLUE).shift(UR*2.7)
         lol = create_glow(c, 12, color=PURE_RED)
         lol2 = create_glow(d, 12, color=PURE_BLUE)
